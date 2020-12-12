e6efb4b7cc7a193a2274c80bf9804a7d6e21240a
Kahil Nayton <kahil@kworq.com>
2020-12-12 14:06:36 -0500

    configured the middleware for server

---
+++ server/middleware/auth.js
@@ -0,0 +1,32 @@
+require("dotenv").load()
+var jwt = require('jsonwebtoken')
+
+exports.loginRequired = function (req, res, next) {
+  try {
+    const token = req.headers.authorization.split(' ')[1]
+    jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
+      if (decoded) {
+        next()
+      } else {
+        return next ({status: 401, message: "You gotta log in first"})
+      }
+    })
+  } catch (e) {
+    return next({status: 401, message: "Please Log in"})
+  }
+}
+
+exports.ensureCorrectUser = function (req, res, next) {
+  try {
+    const token = req.headers.authorization.split(' ')[1]
+    jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
+      if (decoded && decoded.id === req.params.id) {
+        return next()
+      } else {
+        return next({ status: 401, message: "You are unauthorized"})
+      }
+    })
+  } catch (e) {
+    return next({status: 401, message: "You are no allowed in untill you log in"})
+  }
+}
